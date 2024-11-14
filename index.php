c846deeea75d5e633636b85cf6c19c3195fac3b9
Igen <ricsibalogh999@gmail.com>
2024-11-14 12:10:29 +0100

    1.6

--- index.php
+++ index.php
@@ -405,7 +405,7 @@ function isLoggedIn($pdo) {
             <h3>LIFE COACHING</h3>
             <p>Fedezd fel önmagad és valósítsd meg céljaidat szakértő támogatással</p>
             <a href="./kepzeseink.html#life-coaching" class="login-button">Részletek</a>
-            <a href="#" class="login-button">Jelentkezés</a>
+            <a href="./idopont.php" class="login-button">Jelentkezés</a>
         </div>
 
         <div class="service-card" data-aos="fade-up" data-aos-delay="200">
@@ -413,7 +413,7 @@ function isLoggedIn($pdo) {
             <h3>BUSINESS COACHING</h3>
             <p>Fejleszd vezetői készségeidet és vidd sikerre vállalkozásod</p>
             <a href="./kepzeseink.html#business-coaching" class="login-button">Részletek</a>
-            <a href="#" class="login-button">Jelentkezés</a>
+            <a href="./idopont.php" class="login-button">Jelentkezés</a>
         </div>
 
         <div class="service-card" data-aos="fade-up" data-aos-delay="300">
@@ -421,7 +421,7 @@ function isLoggedIn($pdo) {
             <h3>Stresszkezelés és Reziliencia Workshop</h3>
             <p>Oldd meg konfliktusaidat professzionális segítséggel</p>
             <a href="./kepzeseink.html#workshop" class="login-button">Részletek</a>
-            <a href="#" class="login-button">Jelentkezés</a>
+            <a href="./idopont.php" class="login-button">Jelentkezés</a>
         </div>
 
         <div class="service-card" data-aos="fade-up" data-aos-delay="400">
@@ -429,7 +429,7 @@ function isLoggedIn($pdo) {
             <h3>Stresszkezelés és Reziliencia Workshop</h3>
             <p>Csoportos fejlődési lehetőségek inspiráló környezetben</p>
             <a href="./kepzeseink.html#workshop" class="login-button">Részletek</a>
-            <a href="#" class="login-button">Jelentkezés</a>
+            <a href="./idopont.php" class="login-button">Jelentkezés</a>
         </div>
     </section>
 
